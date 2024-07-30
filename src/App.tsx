9ce99997381be22ca30f6a7ead7c9df751d4d5d3
DimitriDiel <diel-d@web.de>
2024-07-30 14:36:07 +0400

    add Lesson_18

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import Homework_15 from "homeworks/Homework_15/Homework_15"
 import Homework_16 from "homeworks/Homework_16/Homework_16"
+import Homework_18 from "homeworks/Homework_18/Homework_18"
 
 import Lesson_16 from "lessons/Lesson_16/Lesson_16"
+import Lesson_18 from "lessons/lesson_18/Lesson_18"
 
 function App() {
   return (
     <>
       {/* <Homework_15 /> */}
       {/* <Lesson_16 /> */}
-      <Homework_16 />
+      {/* <Homework_16 /> */}
+      {/* <Lesson_18 /> */}
+      <Homework_18 />
     </>
   )
 }
